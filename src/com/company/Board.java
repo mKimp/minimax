7872937df124eaae557843e7a7f2f54cc17033de
Kim Ma <pkm@pdx.edu>
2020-03-01 00:48:16 -0800

    winning conditions

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -19,7 +19,7 @@ public class Board {
             System.out.println();
         }
     }
-    public boolean xWin() {
+    public boolean XWin() {
         //vertical win
         if (this.board[0][0] == 'X' && this.board[1][0] == 'X' && this.board[2][0] == 'X')
             return true;
@@ -36,7 +36,40 @@ public class Board {
         if ((this.board[0][0] == 'X' && this.board[1][1] == 'X' && this.board[2][2] == 'X') ||
                 (this.board[0][2] == 'X' && this.board[1][1] == 'X' && this.board[2][0] == 'X'))
             return true;
+        return false;
+    }
+    public boolean OWin() {
+        //vertical win
+        if (this.board[0][0] == 'O' && this.board[1][0] == 'O' && this.board[2][0] == 'O')
+            return true;
+        if (this.board[0][1] == 'O' && this.board[1][1] == 'O' && this.board[2][1] == 'O')
+            return true;
+        if (this.board[0][2] == 'O' && this.board[1][2] == 'O' && this.board[2][2] == 'O')
+            return true;
+        //horizontal win
+        if ((this.board[0][0] == 'O' && this.board[0][1] == 'O' && this.board[0][2] == 'O') || (this.board[1][0] == 'O'
+                && this.board[1][1] == 'O' && this.board[1][2] == 'O') || (this.board[2][0] == 'O' && this.board[2][1] == 'O'
+                && this.board[2][2] == 'O'))
+            return true;
+        // diagnose win
+        if ((this.board[0][0] == 'O' && this.board[1][1] == 'O' && this.board[2][2] == 'O') ||
+                (this.board[0][2] == 'O' && this.board[1][1] == 'O' && this.board[2][0] == 'O'))
+            return true;
 
+        return false;
+    }
+    public boolean isDraw(){
+        for (int i = 0; i < 3; ++i){
+            for (int j = 0; j < 3; ++j){
+                if (board[i][j] == '_')
+                    return false;
+            }
+        }
+        return true;
+    }
+    public boolean isGameOver(){
+        if (XWin() || OWin() || isDraw())
+            return true;
         return false;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
         Board board = new Board();
         board.initialBoard();
         board.displayBoard();
